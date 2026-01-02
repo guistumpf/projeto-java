9898e2680bc0d6622a95f9b1bb2254490e1fa911
Joao Fernandes <guist4@proton.me>
2026-01-02 16:27:20 -0300

    layout?

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Main {
-    static void meu(){
-        
-    }
+  public static void main(String[] args){
+    JFrame frame = new JFrame(); 
+frame.setSize(500, 500);
+frame.setTitle("Contador");
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setResizable(false);
+    frame.setVisible(true);
+    frame.getContentPane().setBackground(new Color(0x121212));
+JButton button = new JButton();
+frame.add(button);
+button.setBounds(85,80,50,30);
+
+ImageIcon image = new ImageIcon("./e1497514ab168e36120bb64668dbc7af.png");
+frame.setIconImage(image.getImage());
+}
+
 }
